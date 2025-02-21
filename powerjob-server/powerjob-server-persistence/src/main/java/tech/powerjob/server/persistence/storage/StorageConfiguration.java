55b2d3e1f8dd44c544ad9587dbaff740fc51c558
jowee <1608262232@qq.com>
2025-02-21 15:21:09 +0800

    feat(storage): 添加对 Xugu 数据库的支持
    - 新增 XuguSeriesDfsService 类实现对 Xugu 数据库的存储支持
    - 更新 application-daily.properties 文件，添加 Xugu 数据库配置
    - 在 pom.xml 中添加 Xugu 相关依赖
    - 修改 StorageConfiguration 类，注册 XuguSeriesDfsService

--- powerjob-server/powerjob-server-persistence/src/main/java/tech/powerjob/server/persistence/storage/StorageConfiguration.java
+++ powerjob-server/powerjob-server-persistence/src/main/java/tech/powerjob/server/persistence/storage/StorageConfiguration.java
@@ -28,6 +28,12 @@ public class StorageConfiguration {
     }
 
     @Bean
+    @Conditional(XuGuSeriesDfsService.XuGuSeriesCondition.class)
+    public DFsService initXuGuDbFs() {
+        return new XuGuSeriesDfsService();
+    }
+
+    @Bean
     @Conditional(PostgresqlSeriesDfsService.PostgresqlSeriesCondition.class)
     public DFsService initPGDbFs() {
         return new PostgresqlSeriesDfsService();
